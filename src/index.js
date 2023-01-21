cc19f1aaed13d065d99220f36737053f3cd8b0e2
AntonBabenko96 <ababenko159@gmail.com>
2023-01-21 15:26:54 +0200

    asd

--- src/index.js
+++ src/index.js
@@ -1,15 +1,22 @@
 import './css/styles.css';
 import { fetchCountries } from './fetchCountries.js';
+import { makeMarkup } from './murkup.js'
+import Notiflix from 'notiflix';
 const debounce = require('lodash.debounce');
 const DEBOUNCE_DELAY = 300;
 const textInput = document.querySelector("#search-box");
+const countryInfo = document.querySelector(".country-info");
+const countryList = document.querySelector(".country-list");
 
-textInput.addEventListener("input", debounce(() => {
-fetchCountries(textInput.value.trim())
+
+textInput.addEventListener("input", debounce(() => { 
+  countryInfo.innerHTML = '';
+  countryList.innerHTML = '';
+    if (textInput.value.trim() === '') {
+        return
+    }
+  fetchCountries(textInput.value.trim()).then((data) => makeMarkup(data)).catch(() => {
+        Notiflix.Notify.failure("Oops, there is no country with that name")
+    })
   }, DEBOUNCE_DELAY)
 );
-
-
-
-
-
---
+++ src/murkup.js
@@ -0,0 +1,41 @@
+const countryInfo = document.querySelector(".country-info");
+const countryList = document.querySelector(".country-list");
+export function makeMarkup(data) {
+    console.log(data)
+  if (data.length >= 10) {
+    return Notiflix.Notify.info("Too many matches found. Please enter a more specific name.")
+  }
+  else if (data.length >= 2 && data.length < 10) {
+    const markupList = data.map((country) => {
+     return `<li>
+                    <img class="flag-little" src="${country.flags.png}" alt="" />
+                    <p class="country-name-little">${country.name}</p>
+                    </li>`
+    }).join('');
+  
+    countryList.insertAdjacentHTML('beforeend', markupList);
+  
+            } else {
+      const markupInfo = data.map((country) => {
+          const arrName = [];
+          const arr = country.languages.forEach(element => {
+              arrName.push(element.name) 
+          });
+     return `<li class="item">
+                    <div class="container">
+                    <img class="flag" src="${country.flags.png}" alt="" />
+                    <p class="country-name">${country.name}</p>
+                    </div>
+                    <p class="capital">Capital: ${country.capital}</p>
+                    <p class="language">Language: ${arrName}</p>
+                    <p class="population">Population: ${country.population}</p>
+                    </li>`}).join('');
+                countryInfo.insertAdjacentHTML('beforeend', markupInfo)
+                
+          }
+};
+
+
+
+
+
